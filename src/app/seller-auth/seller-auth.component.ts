9d17bb263769142a291ae968755912015789e894
abhinav3254 <abhinav3254@gmail.com>
2023-07-25 21:24:29 +0530

    done post of signup

--- src/app/seller-auth/seller-auth.component.ts
+++ src/app/seller-auth/seller-auth.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SellerService } from '../services/seller.service';
 
 @Component({
   selector: 'app-seller-auth',
   templateUrl: './seller-auth.component.html',
   styleUrls: ['./seller-auth.component.css']
 })
-export class SellerAuthComponent {
+export class SellerAuthComponent implements OnInit {
+
+  constructor(private seller: SellerService) { }
+
+  ngOnInit(): void {
+  }
   signUp(data: object): void {
-    console.warn(data)
+    this.seller.userSignUp(data).subscribe((result) => {
+      console.warn(result);
+    });
   }
 }
